61074a574b193c37c5b1ff00c9ad9898de3b2756
TeeSongs <toluwalopeajibade@gmail.com>
2025-06-30 10:18:16 +0100

    checkout restructuring

--- fresh-js/checkout.js
+++ fresh-js/checkout.js
@@ -1,42 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const orderSumary = document.getElementById("orderSummary");
+  const toggle = document.getElementById("menuToggle");
+  const navLinks = document.getElementById("navLinks");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+  const orderSummary = document.getElementById("orderSummary");
   const orderTotal = document.getElementById("orderTotal");
   const checkoutForm = document.getElementById("checkoutForm");
-  const confirmation = document.getElementById("confirmation")
-
+  const confirmation = document.getElementById("confirmation");
 
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  if (cart.length === 0){
-    orderSumary.innerHTML = "<p>Your cart is empty. </p>";
+  if (cart.length === 0) {
+    orderSummary.innerHTML = "<p>Your cart is empty.</p>";
     orderTotal.textContent = "0";
     checkoutForm.querySelector("button").disabled = true;
     return;
   }
-   
 
   let total = 0;
-  orderSumary.innerHTML = cart.map(item => {
+  orderSummary.innerHTML = cart.map(item => {
     const subtotal = item.qty * item.price;
     total += subtotal;
-
     return `
-
-    <div class= "checkout-item">
-       <p><strong>${item.name}</strong> × ${item.qty}</p>
+      <div class="checkout-item">
+        <p><strong>${item.name}</strong> × ${item.qty}</p>
         <p>₦${subtotal.toLocaleString()}</p>
-    <div>
+      </div>
     `;
   }).join("");
 
   orderTotal.textContent = total.toLocaleString();
 
-  checkoutForm.addEventListener("submit", (e) => {
+  checkoutForm.addEventListener("submit", e => {
     e.preventDefault();
-
-  confirmation.classList.remove("hidden");
-  checkoutForm.classList.add("hidden");
-
-  localStorage.removeItem("cart")
+    confirmation.classList.remove("hidden");
+    checkoutForm.classList.add("hidden");
+    localStorage.removeItem("cart");
   });
-});                                   
+});
